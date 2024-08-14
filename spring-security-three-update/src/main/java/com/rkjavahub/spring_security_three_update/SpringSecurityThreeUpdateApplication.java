175373e3bf4899dc151b9611d18049988e433bb7
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-08-14 10:51:07 +0530

    new project added

---
+++ spring-security-three-update/src/main/java/com/rkjavahub/spring_security_three_update/SpringSecurityThreeUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.rkjavahub.spring_security_three_update;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityThreeUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityThreeUpdateApplication.class, args);
+	}
+
+}
---
+++ spring-security-three-update/src/test/java/com/rkjavahub/spring_security_three_update/SpringSecurityThreeUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkjavahub.spring_security_three_update;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityThreeUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
