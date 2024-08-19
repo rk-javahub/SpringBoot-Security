27cfa98f422b7f597f447633106c35c086f415d6
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-08-19 10:54:04 +0530

    Form based auth project added

---
+++ spring-security-form-auth/src/main/java/com/rkjavahub/security/formbasedauth/FormbasedauthApplication.java
@@ -0,0 +1,13 @@
+package com.rkjavahub.security.formbasedauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormbasedauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormbasedauthApplication.class, args);
+	}
+
+}
---
+++ spring-security-form-auth/src/test/java/com/rkjavahub/security/formbasedauth/FormbasedauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkjavahub.security.formbasedauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormbasedauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
