ab446ce15b895c2661ae8dda5fbbed3f89b369cb
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-08-19 11:20:11 +0530

    chnages added

--- spring-security-form-auth/src/main/java/com/rkjavahub/security/formbasedauth/FormbasedauthApplication.java
+++ spring-security-form-auth/src/main/java/com/rkjavahub/security/formbasedauth/FormbasedauthApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FormbasedauthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FormbasedauthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FormbasedauthApplication.class, args);
+    }
 
 }
