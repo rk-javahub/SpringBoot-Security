38defa1ddfde7427bac283c11f6533912cb18e48
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-08-24 09:13:37 +0530

    changes

---
+++ in-memory-auth/src/main/java/com/rkjavahub/security/in_memory_auth/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.rkjavahub.security.in_memory_auth.config;
+
+/*
+Here we are overriding SecurityFilterChain for customization
+@Configuration provides configuration to application context
+
+ */
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // STATELESS means session details not maintained in cookie
+        // http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("Rohit").password("{noop}xuv500").roles("USER").build();
+
+        UserDetails user1 = User.withUsername("Sachin").password("{noop}mrf").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user, user1);
+    }
+
+
+}
