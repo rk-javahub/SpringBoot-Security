7d632502daf8ea8d521b728d10617898e44a7258
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-08-24 20:34:53 +0530

    database auth

---
+++ h2-database-auth/src/main/java/com/rkjavahub/security/h2databaseauth/H2DatabaseAuthApplication.java
@@ -0,0 +1,13 @@
+package com.rkjavahub.security.h2databaseauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DatabaseAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2DatabaseAuthApplication.class, args);
+    }
+
+}
---
+++ h2-database-auth/src/test/java/com/rkjavahub/security/h2databaseauth/H2DatabaseAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkjavahub.security.h2databaseauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DatabaseAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
